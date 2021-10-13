57a4b7fd33f0c9ec84841daa82988fe584b9c3f2
mohammed-Kharma <74590284+mohammed-Kharma@users.noreply.github.com>
2021-10-13 21:59:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,294 @@
+$(function(){
+    $('.mohammeds').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl = $('.mohammeds');
+      owl.owlCarousel();
+      $('.iconOne').click(function() {
+      owl.trigger('next.owl.carousel');
+      })
+      
+      $('.iconTow').click(function() {
+      
+      owl.trigger('prev.owl.carousel', [300]);
+      });
+    //   end silder1
+    // -------------------------------------------------------
+    // ********************************************************
+    $('.mohammeds1').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl2 = $('.mohammeds1');
+      owl2.owlCarousel();
+      $(' .iconOne').click(function() {
+      owl2.trigger('next.owl.carousel');
+      })
+      
+      $(' .iconTow').click(function() {
+      
+      owl2.trigger('prev.owl.carousel', [300]);
+      });
+    //   silder2
+    // --------------------------------------------------------------------
+    // ****************************************************************************
+    $('.mohammeds2').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl21 = $('.mohammeds2');
+      owl21.owlCarousel();
+      $(' .iconOne').click(function() {
+      owl21.trigger('next.owl.carousel');
+      })
+      
+      $(' .iconTow').click(function() {
+      
+      owl21.trigger('prev.owl.carousel', [300]);
+      });
+    //   emd silder3
+    // ------------------------------------------------------------------------
+    // ************************************************************************
+    $('.mohammeds3').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl22 = $('.mohammeds3');
+      owl22.owlCarousel();
+      $('.iconOne').click(function() {
+      owl22.trigger('next.owl.carousel');
+      })
+      
+      $('.iconTow').click(function() {
+      
+      owl22.trigger('prev.owl.carousel', [300]);
+      });
+    //   end 4
+    // -------------------------------------------------------------
+    // ****************************************************************
+    $('.mohammeds4').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl23 = $('.mohammeds4');
+      owl23.owlCarousel();
+      $(' .iconOne').click(function() {
+      owl23.trigger('next.owl.carousel');
+      })
+      
+      $('.iconTow').click(function() {
+      
+      owl23.trigger('prev.owl.carousel', [300]);
+      });
+    //   --------------------------------------------
+    // *********************************************************
+    $('.mohammeds5').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl24 = $('.mohammeds5');
+      owl24.owlCarousel();
+      $(' .iconOne').click(function() {
+      owl24.trigger('next.owl.carousel');
+      })
+      
+      $(' .iconTow').click(function() {
+      
+      owl24.trigger('prev.owl.carousel', [300]);
+      });
+    //   end -----------------------------------------------------------------
+    // ***************************************************************************
+
+    $('.mohammeds6').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl25 = $('.mohammeds6');
+      owl25.owlCarousel();
+      $('.iconOne').click(function() {
+      owl25.trigger('next.owl.carousel');
+      })
+      
+      $('.iconTow').click(function() {
+      
+      owl25.trigger('prev.owl.carousel', [300]);
+      });
+    //   end 6
+    // ---------------------------------------------------
+    
+    $('.mohammeds7').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            800:{
+              items:2,
+              nav:false
+            },
+            1000:{
+                items:4,
+                nav:false,
+                loop:false
+            }
+        }
+      });
+      var owl26 = $('.mohammeds7');
+      owl26.owlCarousel();
+      $(' .iconOne').click(function() {
+      owl26.trigger('next.owl.carousel');
+      })
+      
+      $(' .iconTow').click(function() {
+      
+      owl26.trigger('prev.owl.carousel', [300]);
+      });
+});
